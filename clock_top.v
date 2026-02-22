2f7c1b59abbb77f0fcd0682dd38a334c547cfc45
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-02-22 13:52:15 +0530

    Add files via upload

---
+++ clock_top.v
@@ -0,0 +1,30 @@
+`include "enable_generator.v"
+`include "digital_clock.v"
+module clock_top(
+    input clk,       // 50 MHz
+    input reset,
+    output [7:0] hh,
+    output [7:0] mm,
+    output [7:0] ss,
+    output pm
+);
+
+wire ena_1hz;
+
+enable_generator u1 (
+    .clk(clk),
+    .reset(reset),
+    .ena_1hz(ena_1hz)
+);
+
+digital_clock u2 (
+    .clk(clk),
+    .reset(reset),
+    .ena(ena_1hz),
+    .pm(pm),
+    .hh(hh),
+    .mm(mm),
+    .ss(ss)
+);
+
+endmodule
