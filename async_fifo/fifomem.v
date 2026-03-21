55114ec10c239a78605bf7fc46fa854ae9b3cdc8
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-21 15:11:56 +0530

    Change ADDR_WIDTH parameter from 8 to 4

--- async_fifo/fifomem.v
+++ async_fifo/fifomem.v
@@ -1,6 +1,6 @@
 module fifomem #(
     parameter DATA_WIDTH = 8,
-    parameter ADDR_WIDTH = 8
+    parameter ADDR_WIDTH = 4
 )(
     input w_clk,
     input w_en,
@@ -12,7 +12,6 @@ module fifomem #(
     input [ADDR_WIDTH-1:0] r_addr,
     output reg [DATA_WIDTH-1:0] r_data,
 );
-
 localparam RAM_DEPTH = 1 << ADDR_WIDTH;
 
 reg [DATA_WIDTH-1:0] mem [0:RAM_DEPTH-1];
@@ -24,5 +23,4 @@ end
 always @ (posedge r_clk) begin:read
     if(R_en) r_data <= mem[r_addr];
 end
-
 endmodule
