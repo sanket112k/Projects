1446589170df1960bbe529d512d24e9cf524ef36
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-21 17:13:45 +0530

    Refactor FIFO memory module input signals

--- async_fifo/fifomem.v
+++ async_fifo/fifomem.v
@@ -1,26 +1,27 @@
-module fifomem #(
+// FIFO memory buffer
+module fifo_mem #(
     parameter DATA_WIDTH = 8,
     parameter ADDR_WIDTH = 4
 )(
-    input w_clk,
-    input w_en,
-    input [ADDR_WIDTH-1:0] w_addr,
-    input [DATA_WIDTH-1:0] w_data,
+    input wclk,
+    input wen,
+    input [ADDR_WIDTH-1:0] waddr,
+    input [DATA_WIDTH-1:0] wdata,
 
-    input r_clk,
-    input r_en,
-    input [ADDR_WIDTH-1:0] r_addr,
-    output reg [DATA_WIDTH-1:0] r_data,
+    input rclk,
+    input ren,
+    input [ADDR_WIDTH-1:0] raddr,
+    output reg [DATA_WIDTH-1:0] rdata,
 );
 localparam RAM_DEPTH = 1 << ADDR_WIDTH;
 
 reg [DATA_WIDTH-1:0] mem [0:RAM_DEPTH-1];
 
 always @ (posedge w_clk) begin:write
-   if (w_en) mem[w_addr] <= w_data;
+    if (wen) mem[waddr] <= wdata;
 end
 
 always @ (posedge r_clk) begin:read
-    if(R_en) r_data <= mem[r_addr];
+    if(ren) rdata <= mem[raddr];
 end
 endmodule
