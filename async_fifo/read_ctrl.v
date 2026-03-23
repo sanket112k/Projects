374a2b2a01bcbad6e5c1abaaac7bb344a32f7e95
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-23 23:59:38 +0530

    Add files via upload

---
+++ async_fifo/read_ctrl.v
@@ -0,0 +1,58 @@
+// Read pointer & empty generation logic
+module read_ctrl #(
+    parameter ADDR_WIDTH = 4
+)(
+    input rclk,
+    input rreset,
+    input ren,
+    input      [ADDR_WIDTH:0]   wptr_sync,
+
+    output     [ADDR_WIDTH-1:0] raddr,
+    output reg [ADDR_WIDTH:0]   rptr_gray,
+    output reg empty,
+    output reg rvalid
+);
+reg [ADDR_WIDTH:0] rptr_bin;
+reg [ADDR_WIDTH:0] rgnext;
+reg [ADDR_WIDTH:0] rbnext;
+reg ren_d;
+
+always @(posedge rclk) begin:next_transition
+    if (rreset) begin
+        rptr_bin  <= 0;
+        rptr_gray <= 0;
+    end
+    else begin
+        rptr_bin  <= rbnext;
+        rptr_gray <= rgnext;
+    end
+end
+/*
+always @(posedge rclk) begin
+    if (rreset)
+        ren_d <= 0;
+    else
+        ren_d <= ;
+end
+*/
+always @(*) begin:ptr_inc_logic
+    rbnext = rptr_bin + ren & ~empty;
+    rgnext = (rbnext>>1) ^ rbnext;
+end
+
+assign raddr = rptr_bin[ADDR_WIDTH-1:0];
+
+always @(posedge rclk) begin:empty_flag
+    if (rreset)
+        empty <= 1;
+    else
+        empty <= (rgnext == wptr_sync);
+end
+
+always @(posedge rclk) begin:valid_generatoion
+    if (rreset)
+        rvalid <= 0;
+    else
+        rvalid <= ren & ~empty;
+end
+endmodule
