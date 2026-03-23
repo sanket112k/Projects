bf3ca14d2231a6d8cab75fdcd39f82d37d89ee47
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-23 23:57:32 +0530

    Add files via upload

---
+++ fifo_mem.v
@@ -0,0 +1,26 @@
+// FIFO memory buffer
+module fifo_mem #(
+    parameter DATA_WIDTH = 8,
+    parameter ADDR_WIDTH = 4
+)(
+    input wclk,
+    input wen,
+    input [ADDR_WIDTH-1:0] waddr,
+    input [DATA_WIDTH-1:0] wdata,
+
+    input rclk,
+    input ren,
+    input [ADDR_WIDTH-1:0] raddr,
+    output reg [DATA_WIDTH-1:0] rdata
+);
+localparam RAM_DEPTH = 1 << ADDR_WIDTH;
+
+reg [DATA_WIDTH-1:0] mem [0:RAM_DEPTH-1];
+
+always @(posedge wclk)
+    if (wen) mem[waddr] <= wdata;
+
+always @(posedge rclk)
+    rdata <= mem[raddr];
+
+endmodule
